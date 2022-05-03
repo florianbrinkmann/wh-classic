28062c83382d6071d8d25d224b988532d06e58f4
Florian Brinkmann <kontakt@florianbrinkmann.com>
2022-05-03 12:25:34 +0200

    ✨ adapt content-page.php template part for search context

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -7,12 +7,21 @@
 <article <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		the_title( '<h1 class="entry-title">', '</h1>' );
+		if ( is_search() ) {
+			$permalink = get_the_permalink();
+			the_title( "<h2 class='entry-title'><a href='$permalink'>", "</a></h2>" );
+		} else {
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		}
 		?>
 	</header>
 	<div class="entry-content">
 		<?php
-		the_content();
+		if ( is_search() ) {
+			the_excerpt();
+		} else {
+			the_content();
+		}
 		?>
 	</div>
 </article>
